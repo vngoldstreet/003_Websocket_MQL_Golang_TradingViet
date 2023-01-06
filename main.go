a9aeb78c60a4a4a549bf3c852f849a4b71cc122f
vietvufx <vietvufx@gmail.com>
2023-01-06 17:43:24 +0700

    add indicator and ea with new format

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -29,26 +28,38 @@ var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
 
 func main() {
 	router := gin.Default()
-	router.POST("/api/orders", apiOrderRequest)
 	router.GET("/", func(c *gin.Context) {
 		DataStreamFromMQL5(c.Writer, c.Request)
 	})
 	router.GET("/ws", func(c *gin.Context) {
 		StreamDatas(c.Writer, c.Request)
 	})
-	router.Run(":8080")
+	router.Run(":8802")
 }
 
-type Data struct {
-	XMLName   xml.Name `xml:"data"`
-	Text      string   `xml:",chardata"`
-	Name      string   `xml:"name"`
-	ID        int64    `xml:"id"`
-	Balance   float64  `xml:"balance"`
-	Equity    float64  `xml:"equity"`
-	PosProfit float64  `xml:"pos_profit"`
-	HisProfit float64  `xml:"his_profit"`
-	PerProfit float64  `xml:"per_profit"`
+type DataEa struct {
+	XMLName xml.Name `xml:"data_ea"`
+	Text    string   `xml:",chardata"`
+	Item    []struct {
+		Text      string  `xml:",chardata"`
+		ID        int     `xml:"id,attr"`
+		Name      string  `xml:"name,attr"`
+		Balance   float64 `xml:"balance,attr"`
+		Equity    float64 `xml:"equity,attr"`
+		PosProfit float64 `xml:"pos_profit,attr"`
+		HisProfit float64 `xml:"his_profit,attr"`
+		PerProfit float64 `xml:"per_profit,attr"`
+	} `xml:"item"`
+}
+
+type DataIndi struct {
+	XMLName xml.Name `xml:"data_indicator"`
+	Text    string   `xml:",chardata"`
+	Item    []struct {
+		Text     string  `xml:",chardata"`
+		IndiName string  `xml:"indi_name,attr"`
+		Data     float64 `xml:"data,attr"`
+	} `xml:"item"`
 }
 
 func DataStreamFromMQL5(w http.ResponseWriter, r *http.Request) {
@@ -63,27 +74,52 @@ func DataStreamFromMQL5(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			break
 		}
-		data := Data{}
-		xml.Unmarshal(msg, &data)
-		message = []DataStreams{
-			{
-				// Name:      data.Name,
-				ID:        data.ID,
-				Balance:   data.Balance,
-				Equity:    data.Equity,
-				PosProfit: data.PosProfit,
-				HisProfit: data.HisProfit,
-				PerProfit: data.PerProfit,
-			},
+		fmt.Printf("msg: %s\n", msg)
+
+		data_resp := DataEa{}
+		data_indi_resp := DataIndi{}
+
+		if err_mars := xml.Unmarshal(msg, &data_resp); err_mars == nil {
+			data := []DataStreamEAs{}
+			for _, v := range data_resp.Item {
+				cur := DataStreamEAs{
+					ID:        v.ID,
+					Name:      v.Name,
+					Balance:   v.Balance,
+					Equity:    v.Equity,
+					HisProfit: v.HisProfit,
+					PerProfit: v.PerProfit,
+				}
+				data = append(data, cur)
+			}
+			ea_data = data
 		}
-		tick = time.NewTicker(time.Millisecond * 100)
+
+		if err_mars := xml.Unmarshal(msg, &data_indi_resp); err_mars == nil {
+			data := []DataStreamIndis{}
+			for _, v := range data_indi_resp.Item {
+				cur := DataStreamIndis{
+					Name: v.IndiName,
+					Data: v.Data,
+				}
+				data = append(data, cur)
+			}
+			indi_data = data
+		}
+
 		conn.WriteMessage(t, msg)
+		// tick = time.NewTicker(time.Millisecond * 100)
 	}
 }
 
-type DataStreams struct {
-	// Name      string  `json:"name"`
-	ID        int64   `json:"id"`
+type DataStreamIndis struct {
+	Name string  `json:"name"`
+	Data float64 `json:"data"`
+}
+
+type DataStreamEAs struct {
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
 	Balance   float64 `json:"balance"`
 	Equity    float64 `json:"equity"`
 	PosProfit float64 `json:"pos_profit"`
@@ -91,7 +127,13 @@ type DataStreams struct {
 	PerProfit float64 `json:"per_profit"`
 }
 
-var message []DataStreams
+type DataStreams struct {
+	DataStreamEAs   []DataStreamEAs
+	DataStreamIndis []DataStreamIndis
+}
+
+var ea_data []DataStreamEAs
+var indi_data []DataStreamIndis
 
 var tick *time.Ticker
 
@@ -102,12 +144,17 @@ func StreamDatas(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
+	tick = time.NewTicker(time.Second)
 	quit := make(chan struct{})
 	for {
 		select {
 		case <-tick.C:
 			{
-				conn.WriteJSON(message)
+				datastream := DataStreams{
+					DataStreamEAs:   ea_data,
+					DataStreamIndis: indi_data,
+				}
+				conn.WriteJSON(datastream)
 			}
 		case <-quit:
 			tick.Stop()
@@ -125,34 +172,4 @@ type Orders struct {
 	Volume     float64 `json:"volume"`
 	CPrice     float64 `json:"cprice"`
 	CProfit    float64 `json:"cprofit"`
-}
-
-var res Orders
-
-func apiOrderRequest(c *gin.Context) {
-	token := c.DefaultQuery("token", "")
-	if token == "7884d9ca4d43403acc22f51b597ad15fbc0cac34d2625fef9b3e8be8a25faffa" {
-		id := c.PostForm("id")
-		side := c.PostForm("side")
-		price, _ := strconv.ParseFloat(c.PostForm("price"), 64)
-		stoploss, _ := strconv.ParseFloat(c.PostForm("stoploss"), 64)
-		takeprofit, _ := strconv.ParseFloat(c.PostForm("takeprofit"), 64)
-		volume, _ := strconv.ParseFloat(c.PostForm("volume"), 64)
-		cprice, _ := strconv.ParseFloat(c.PostForm("cprice"), 64)
-		cprofit, _ := strconv.ParseFloat(c.PostForm("cprofit"), 64)
-		res = Orders{
-			ID:         id,
-			Side:       side,
-			Price:      price,
-			Stoploss:   stoploss,
-			TakeProfit: takeprofit,
-			Volume:     volume,
-			CPrice:     cprice,
-			CProfit:    cprofit,
-		}
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": "Wrong token!",
-		})
-	}
 }
